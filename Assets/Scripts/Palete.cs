fb0371e1eb7a8c11e17b3703e9bbe98007adcb33
majakthecoder <majewskis@gmail.com>
2021-10-13 00:35:36 -0700

    Racket mechanics

--- Assets/Scripts/Palete.cs
+++ Assets/Scripts/Palete.cs
@@ -12,14 +12,21 @@ public class Palete : MonoBehaviour
         Shot,
     }
 
-    public GameObject palete;
+    public GameObject racket;
+    public GameObject racketReference;
+    public GameObject racketPivot;
 
     private State state;
+    private Vector3 initialUp;
+    private float swing;
+    private const float kAngleAcceleration = 10000.0f;
+    private float swingAngleSpeed = 0.0f;
+    private float prevShotAngle;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -32,8 +39,12 @@ public class Palete : MonoBehaviour
                 {
                     Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                     position.z = 0.0f;
-                    palete.transform.position = position;
+                    transform.position = position;
                     state = State.Position;
+
+                    racket.SetActive(false);
+                    racketReference.SetActive(false);
+                    racketPivot.SetActive(true);
                 }
                 break;
 
@@ -42,8 +53,13 @@ public class Palete : MonoBehaviour
                 {
                     Vector3 touchWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                     touchWorldPosition.z = 0.0f;
-                    palete.transform.up = (touchWorldPosition - palete.transform.position).normalized;
+                    transform.up = (touchWorldPosition - transform.position).normalized;
+                    initialUp = transform.up;
                     state = State.Angle;
+
+                    racket.SetActive(true);
+                    racketReference.SetActive(true);
+                    racketPivot.SetActive(false);
                 }
                 break;
 
@@ -52,15 +68,46 @@ public class Palete : MonoBehaviour
                 {
                     Vector3 touchWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                     touchWorldPosition.z = 0.0f;
-                    palete.transform.up = (touchWorldPosition - palete.transform.position).normalized;
+
+                    transform.up = (touchWorldPosition - transform.position).normalized;
+
+                    if ((transform.up - initialUp).sqrMagnitude > Vector3.kEpsilon)
+                    {
+                        float angleSign = Vector3.SignedAngle(transform.up, initialUp, Vector3.forward);
+                        if (Mathf.Abs(angleSign) <= 90.0f)
+                        {
+                            swing = angleSign;
+                        }
+
+                        if (Mathf.Sign(angleSign) != Mathf.Sign(swing))
+                        {
+                            transform.up = -initialUp;
+                        }
+
+                        Debug.Log($"angle={Vector3.SignedAngle(transform.up, initialUp, Vector3.forward)}, swing={swing}");
+                    }
                 }
                 else if (Input.GetMouseButtonUp(0))
                 {
+                    prevShotAngle = Vector3.Angle(transform.up, initialUp);
                     state = State.Shot;
+                    swingAngleSpeed = 0.0f;
                 }
                 break;
 
             case State.Shot:
+                swingAngleSpeed += kAngleAcceleration * Time.deltaTime;
+                transform.up = Quaternion.AngleAxis(swingAngleSpeed * Time.deltaTime * Mathf.Sign(swing), Vector3.forward) * transform.up;
+
+                float angle = Vector3.Angle(transform.up, initialUp);
+
+                if (Mathf.Abs(angle) < 0.1 || Mathf.Abs(angle) > Mathf.Abs(prevShotAngle))
+                {
+                    transform.up = initialUp;
+                    state = State.Idle;
+                }
+
+                prevShotAngle = angle;
 
                 break;
 
